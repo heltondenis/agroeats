551de6da1037fe01d6f77661b8db2ed5754cac1d
Cleiton Bonomini <cleiton.bonomini@gmail.com>
2019-03-30 22:53:36 -0300

    Configuração de seeds e  migrations

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Prestador 01',
+            'email' => 'prestador01'.'@gmail.com',
+            'password' => bcrypt('prestador123'),
+            'is_prestador' => 1
+        ]);
+    }
+}
